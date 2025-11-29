f5f814dec15970a2484fc8aca42fc7f1ae6ad87a
scx1332 <sieciech@gmail.com>
2025-11-29 20:03:13 +0100

    Working on page test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/arkiv-page-test/static/",
   optimizeDeps: {
     exclude: ["brotli-wasm", "brotli-wasm/pkg.bundler/brotli_wasm_bg.wasm"],
   },
